972697216f20f96ce8d782351238aa1e376e3bf2
ManvitSai1 <92364927+ManvitSai1@users.noreply.github.com>
2024-05-07 16:35:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function setup()
+{
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+
+status ="";
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    document.getElementById('input').value
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+}
+function draw(){
+    image(video, 0, 0, 480, 380);
+
+}
